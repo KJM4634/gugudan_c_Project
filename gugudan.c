d649f34267668b4d96e80dd2debd9a7deae70f52
KimJeongMin <ssjm524@naver.com>
2025-10-30 20:51:26 +0900

    단이름 오류 수정

--- gugudan.c
+++ gugudan.c
@@ -3,11 +3,11 @@
 int main(){
     printf("--- 구구단 출력 ---\n");
     for(int i=2;i<=9;i++){
-        printf("\n----%d 단----\n");
+        printf("\n----%d 단----\n",i);
         for(int j=1;j<=9;j++){
             printf("%d X %d = %d\n",i,j,i*j);
         }
     }
-    printf("---구구단 끝---");
+    printf("---구구단 끝---\n");
     return 0;
 }
